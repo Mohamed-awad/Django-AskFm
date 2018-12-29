fd4bbb1ce80c912bdf284b1ca4a4f99bd2a56820
Mohamed Awad <awadmohamed233@gmail.com>
2018-12-29 19:01:10 +0200

    finish front end for questions page

--- askfm/question/forms.py
+++ askfm/question/forms.py
@@ -3,6 +3,13 @@ from .models import Question
 
 
 class QuestionCreateForm(forms.ModelForm):
+
   class Meta:
     model = Question
-    fields = ['answer', 'status']
+    fields = ['body', 'answer', 'status']
+
+  def __init__(self, *args, **kwargs):
+    super(QuestionCreateForm, self).__init__(*args, **kwargs)
+    self.fields['body'].label = 'Question'
+    self.fields['body'].widget.attrs['readonly'] = True
+    self.fields['body'].widget.attrs['rows'] = '1'
